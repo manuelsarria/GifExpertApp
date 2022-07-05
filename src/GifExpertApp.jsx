e1687f377d3abe22d83fb0b55a4ae2dd2b8851c1
Manuel Sarrria <manuelsarria@Manuels-MacBook-Pro.local>
2022-07-05 14:36:21 -0500

    add component to add category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  const onAddCategory = (newCategory) => {
+
+    setCategories([newCategory, ...categories])
+    // setCategories( cat => [...cat, 'Valorant']);
+
+  }
+
   return (
     <>
       {/* Titulo */}
       <h1>GifExpertApp</h1>
 
       {/* Input */}
+      <AddCategory 
+        // setCategories={setCategories}
+        onNewCategory={ (value) => onAddCategory(value)}
+      />
 
       {/* listado de gif */}
+      <ol>
+        {
+            categories.map((category) => {
+             return <li key={ category }>{category}</li>
+            })
+        }
+      </ol>
 
       {/* Gif items */}
     </>
-  )
+  );
 }
